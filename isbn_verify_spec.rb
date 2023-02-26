c61e80ce3547e266d88c5def374951ba5fe54b3f
Carlos Edilson Junior <40894261+carlosedilsonjr@users.noreply.github.com>
2023-02-25 23:20:06 -0300

    update rspec

--- isbn_verify_spec.rb
+++ isbn_verify_spec.rb
@@ -1,33 +1,53 @@
 require_relative 'isbn_verify'
 
-RSpec.describe "Isbn13 Verify" do
-  it "return TypeError when initialize with string" do
-    isbn = '978014300723'
-    expect{Isbn13.new(isbn)}.to raise_error(TypeError)
+RSpec.describe "Isbn13" do
+  subject { Isbn13.new(isbn) }
+
+  context "when initialize with string" do
+    let(:isbn) { '9780143007235' }
+
+    it "throws TypeError" do
+      expect{subject}.to raise_error(TypeError)
+    end
   end
 
-  it "return ArgumentError when initialize with more than 13 digits" do
-    isbn = 97801430072345
-    expect{Isbn13.new(isbn)}.to raise_error(ArgumentError)
+  context "when initialize with more than 13 digits" do
+    let(:isbn) { 97801430072345 }
+
+    it "throws ArgumentError" do
+      expect{subject}.to raise_error(ArgumentError)
+    end
   end
 
-  it "return ArgumentError when initialize with less than 12 digits" do
-    isbn = 97801430072
-    expect{Isbn13.new(isbn)}.to raise_error(ArgumentError)
+  context "when initialize with less than 12 digits" do
+    let(:isbn) { 97801430072 }
+
+    it "throws ArgumentError" do
+      expect{subject}.to raise_error(ArgumentError)
+    end
   end
 
-  it "returns VALID for a valid ISBN-13" do
-    isbn = 9783161484100
-    expect(Isbn13.new(isbn).verify).to eq('VALID')
+  context "when initialize with a valid 13 digits number" do
+    let(:isbn) { 9780143007234 }
+
+    it "returns VALID" do
+      expect(subject.verify).to eq('VALID')
+    end
   end
 
-  it "returns INVALID for an invalid ISBN-13" do
-    isbn = 9783161484101
-    expect(Isbn13.new(isbn).verify).to eq('INVALID')
+  context "when initialize with an invalid 13 digits number" do
+    let(:isbn) { 9780143007235 }
+
+    it "returns INVALID" do
+      expect(subject.verify).to eq('INVALID')
+    end
   end
 
-  it "returns the check digit for a 12 digits number" do
-    isbn = 978014300723
-    expect(Isbn13.new(isbn).verify).to eq(9780143007234)
+  context "when initialize with a 12 digits number" do
+    let(:isbn) { 978014300723 }  
+
+    it "returns the same number with the check digit as last digit" do
+      expect(subject.verify).to eq(9780143007234)
+    end
   end
 end
